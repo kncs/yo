4eb90dfce8ac9b4d24c1b214a27c7dfe11fd9794
Kevin Clarens <k.clarens@adveez.com>
2017-12-13 18:57:01 +0100

    feat(font-awesome): add font awesome dependency

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Overview from '@/components/overview'
 
+import 'font-awesome/css/font-awesome.css'
+
 Vue.use(Router)
 
 export default new Router({
